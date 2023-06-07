21f0498bc0907cd00d5a1acdeb8e341c7826035a
camilo-camargo <camilocamargo49@gmail.com>
2023-06-06 21:04:43 -0500

    Add fastify and express framework's bootstrap.

--- packages/learning/src/main.ts
+++ packages/learning/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+
+async function express_bootstrap(): Promise<INestApplication>{
+  return await NestFactory.create<NestExpressApplication>(AppModule);
+}
+
+async function fastify_bootstrap(): Promise<INestApplication>{
+  return await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await express_bootstrap();
+  //const app = await fastify_bootstrap();
   await app.listen(3000);
 }
 bootstrap();
